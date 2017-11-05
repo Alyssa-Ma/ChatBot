41e6c82d4f8fea37c99e8980ac361bebb6c99433
pjdan <pjdan@DESKTOP-5CQOPLM.fios-router.home>
2017-11-05 16:53:04 -0500

    FIXED SOME ERRORS

--- ChatBot/src/DLiBot.java
+++ ChatBot/src/DLiBot.java
@@ -10,7 +10,7 @@ public class DLiBot
 {
 	String product = "";
 	
-	public String returnToMianMenu(String statement) { 
+	public String returnToMainnMenu(String statement) { 
 		String[] closingResponse = {"bye","Bye", "want to leave","I'm out","main menu"};
 		String closure = "";
 		for(int i = 0; i< closingResponse.length; i++) {
@@ -232,59 +232,8 @@ public class DLiBot
 	{
 		if(age<=18&&findKeyword(condition.toLowerCase(), "oil")>=0)
 		{
-			String product= "oily";
-			
 			return "You have an oily skin type. For the products recommended, please be cautious as you are still young and your skin is sensitive.";
 		}
-	}
-	/**
-	 * Gives a response to a user statement
-	 * 
-	 * @param statement
-	 *            the user statement
-	 * @return a response based on the rules given
-	 */
-	//public String getResponse(String statement)
-	{
-		String response = "";
-		
-		if (statement.length() == 0)
-		{
-			response = "Say something, please.";
-		}
-
-		else if (findKeyword(statement, "no") >= 0)
-		{
-			response = "Why so negative?";
-                	emotion--;
-		}
-		
-		else if (findKeyword(statement, "levin") >= 0)
-		{
-			response = "More like LevinTheDream amiright?";
-			emotion++;
-		}
-
-		// Response transforming I want to statement
-		else if (findKeyword(statement, "I want to", 0) >= 0)
-		{
-			response = transformIWantToStatement(statement);
-		}
-		else if (findKeyword(statement, "I want",0) >= 0)
-		{
-			response = transformIWantStatement(statement);
-		}	
-		else
-		{
-			response = getRandomResponse();
-		}
-		
-		return response;
-	}
-	
-	
-
-	
 	
 	/**
 	 * Search for one word in phrase. The search is not case
@@ -369,24 +318,24 @@ public class DLiBot
 	 * Pick a default response to use if nothing else fits.
 	 * @return a non-committal string
 	 */
-	private String getProducts ()
+	private String[] getProducts ()
 	{
-		Random r = new Random ();
-		if (product.equals("oily"))
-		{	
-			System.out.println(NeutralResponses);
-		}
-		if (emotion < 0)
-		{	
-			return randomAngryResponses [r.nextInt(randomAngryResponses.length)];
-		}	
-		return randomHappyResponses [r.nextInt(randomHappyResponses.length)];
+		return Products;
 	}
 	
-	private String [] NeutralResponses = {"1.Double cleansing is a must",
+	private String [] Products = {"Here is my recommended nighttime routine.\r\n" + 
+			"Warning:Please take caution when using these products. These products will not work for everyone. Always try a little bit on your skin to see how you react to it first. These products have shown results to many but it may not work for you. Do not expect a miracle. Besides these products, patience and living a healthy lifestyle will improve your skin health and allow the products to work better. \r\n" + 
+			"1.Double cleansing is a must especially if you put on makeup. Start off with a cleansing balm or cleansing oil. I recommend using the Heimish All Clean Balm or Manyo Factory Cleansing Oil.\r\n" + 
+			"2.After removing all the excess oil, makeup, and dirt off your face, use a gentle low PH cleanser. Low PH cleansers are gentle on the face, since our face generally has a low PH naturally. For oily skin, to avoid breakouts and clogged pores, use a low PH salicylic acid cleanser or other cleansers with BHA (beta hydroxy acids). BHAs help deep cleanse your pores on the inside and remove the dirt from within the pores. Tea tree oil in cleansers also helps calm acne. I recommend the CosrX Salicylic Acid Cleanser or Dr.G Clear Foam Cream. For dry skin types, a cleanser should not strip away your moisture levels and give you that squeaky clean feeling. I recommend the CosrX Low PH Morning Gel Cleanser or the Glossier Milky Jelly Cleanser. For normal skin types, I would recommend just using any cleanser with a low PH and is gentle on your skin. I would recommend the Klavuu Pearl Cleansing Foam. For mature skin types, higher end brands tend to work better because of the anti-aging properties. A very popular anti-aging ingredient is ginseng. This can be seen in the Sulwhasoo cleanser. After cleansing, pat the water in your face. Drying your face with a towel or paper towel is not ideal. You want to make sure your face is hydrated and to avoid transepidermal water loss.\r\n" + 
+			"3.Once a week, exfoliate your skin so that your skin will have a smoother surface and so that products can easily penetrate in. Exfoliating helps clear the layer of dead skin cells on your face. Exfoliating too often will cause irritation to the skin. I recommend using SkinFood sugar scrub. However, scrubs can be too sensitive for some people so peeling pads work as well. I recommend NeoGen peeling pads or Dr.Grand+ Milk Moisture Pads. Another option are peeling gels/gommage type exfoliators like the Innisfree Gommage Mask and the Secret Key Lemon Peeling Gel.\r\n" + 
+			"4.For oily skin types especially, including a clay mask at this step will help absorb the excess sebum in your pores, make your pores appear smaller, and give your skin a smoother surface. I recommend the Innisfree Mousse Clay Mask or the Glossier Galaxy Mask.\r\n" + 
+			"5. After cleansing comes hydration, it is important for every skin type to stay hydrated as it is like drinking water for the body everyday. Apply a toner. Toner helps balance the PH level of your skin and provides extra hydration. I recommend the Pyunkang Yul Essence Toner or the Isntree Hylauronic Acid Toner. Pat this in your face. DO NOT RUB. Use your hands or a cotton pad. When looking for hydrating products look for ingredients like aloe, hylauronic acid, etc. Also look for humectants like glycerin.\r\n" + 
+			"4. This step is optional but recommended if your skin is especially dry and dehydrated. I recommend using an essence like the Secret Key essence, SKII essence or Missha essence. Pat this in your face. DO NOT RUB. Use your hands or a cotton pad. \r\n" + 
+			"5.The products I am about to mention are not for everyone as these can be extremely sensitive to your skin. Always stick to one of these products at a time to not over stress the skin. Pigmentation/acne scars result from a overproduction of melanin to protect the wound. Therefore, “shedding” the skin layer by layer will help remove that excess melanin. Retinol would be a good ingredient to use. However, please show caution when using this product. Retinol is not for everyone and it could have serious side effects. Retinol also tends to dehydrate your skin. I recommend the Differin Gel. Use at your own discretion. Another ingredient would be Vitamin C. Vitamin C brightens up your complexion, battles pigmentation, and can even out skin tone. I recommend using the Cosrx Triple C Lightning Liquid. For acne, use a clindamycin gel prescribed by a dermatologist. This will help prevent the growth of bacteria as this is what clogs up your pores to begin with that cause acne. \r\n" + 
+			"6. Now seal all the products you just put on by using a moisturizer. I recommend using Cosrx snail cream or the Hado Labo hylauronic acid lotion. \r\n" + 
+			"7. The last step is eye cream. The eye skin is the thinnest skin on your body. Therefore, using a moisturizer specific to that part of the eye is necessary. I recommend the Innisfree Orchid Eye Cream or the Clinique Eye Cream.\r\n" + 
+			"You can remove steps as you wish because there are a lot but these products have shown results.\r\n" 
 			
 	};
-	private String [] randomAngryResponses = {"Bahumbug.", "Harumph", "The rage consumes me!"};
-	private String [] randomHappyResponses = {"H A P P Y, what's that spell?", "Today is a good day", "You make me feel like a brand new pair of shoes."};
 	
 }
